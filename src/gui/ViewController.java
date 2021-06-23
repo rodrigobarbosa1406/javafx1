54433789ddd0999a5cc4852c17c448cbecb2e0c7
rodrigobarbosa1406 <rodrigobarbosanb@hotmail.com>
2021-06-22 21:43:31 -0300

    ComboBox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,17 +1,26 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import gui.util.Alerts;
 import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Pessoa;
 
 public class ViewController implements Initializable {
 
@@ -29,6 +38,14 @@ public class ViewController implements Initializable {
 
 	@FXML
 	private Button btSoma;
+
+	@FXML
+	private Button btTudo;
+	
+	@FXML
+	private ComboBox<Pessoa> comboBoxPessoa;
+
+	private ObservableList<Pessoa> obsList;
 
 	@FXML
 	public void onBtTestAction() {
@@ -50,12 +67,44 @@ public class ViewController implements Initializable {
 			Alerts.showAlert("Erro", "Parse error", e.getMessage(), AlertType.ERROR);
 		}
 	}
+	
+	@FXML
+	public void onComboBoxPessoaAction() {
+		Pessoa pessoa = comboBoxPessoa.getSelectionModel().getSelectedItem();
+		System.out.println(pessoa);
+	}
+	
+	@FXML
+	public void onBtTudoAction() {
+		for (Pessoa pessoa : comboBoxPessoa.getItems()) {
+			System.out.println(pessoa);
+		}
+	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		Constraints.setTextFieldDouble(txtNumero1);
 		Constraints.setTextFieldDouble(txtNumero2);
+
 		Constraints.setTextFieldMaxLength(txtNumero1, 12);
 		Constraints.setTextFieldMaxLength(txtNumero2, 12);
+
+		List<Pessoa> lstPessoa = new ArrayList<>();
+		lstPessoa.add(new Pessoa(1, "Maria", "maria@gmail.com"));
+		lstPessoa.add(new Pessoa(2, "Alex", "alex@gmail.com"));
+		lstPessoa.add(new Pessoa(3, "Bob", "bob@gmail.com"));
+
+		obsList = FXCollections.observableArrayList(lstPessoa);
+		comboBoxPessoa.setItems(obsList);
+
+		Callback<ListView<Pessoa>, ListCell<Pessoa>> factory = lv -> new ListCell<Pessoa>() {
+			@Override
+			protected void updateItem(Pessoa item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getNome());
+			}
+		};
+		comboBoxPessoa.setCellFactory(factory);
+		comboBoxPessoa.setButtonCell(factory.call(null));
 	}
 }
